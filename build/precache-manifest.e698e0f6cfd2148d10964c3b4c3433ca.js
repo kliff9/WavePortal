087daee1a7a6a3952188a81fa3d371068d391d30
kliff9 <c.thelusca@hotmail.com>
2022-01-21 00:05:01 -0500

    commit message

---
+++ build/precache-manifest.e698e0f6cfd2148d10964c3b4c3433ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32abae96308fd36f342fbfcd9c3cdfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd33281dbdb6fb0afde1",
+    "url": "/static/css/main.80cdeb61.chunk.css"
+  },
+  {
+    "revision": "35b5f192a57198c81710",
+    "url": "/static/js/2.2010fc6a.chunk.js"
+  },
+  {
+    "revision": "bd33281dbdb6fb0afde1",
+    "url": "/static/js/main.a48b2756.chunk.js"
+  },
+  {
+    "revision": "dd4f5311301ce6943c32",
+    "url": "/static/js/runtime-main.5fe07a7e.js"
+  }
+]);
